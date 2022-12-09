162cb097916942d63b4bc370ee265d717708c0fa
jmann1985 <116019572+jmann1985@users.noreply.github.com>
2022-12-09 12:19:22 -0500

    Create Final Project Decision Tree R Studio.r

---
+++ Final Project Decision Tree R Studio.r
@@ -0,0 +1,13 @@
+set.seed(69)
+trainIndex <- createDataPartition(MonkeyPox$MonkeyPox, p=0.8, list = FALSE)
+Train <- MonkeyPox[ trainIndex, ]
+Test <- MonkeyPox[ -trainIndex, ]
+treemodel <- rpart(MonkeyPox~., data = Train)
+treeplot <- rpart.plot(treemodel)
+treemodel$variable.importance
+Prediction <- predict(treemodel, Test, type = 'class')
+Prediction
+Confmatrix <- Table(Test$MonkeyPox, Prediction)
+Confmatrix
+Acc <- (sum(diag(Confmatrix)) / sum(Confmatrix)*100)
+Acc
